bae8cde1add575e72564b83f845131061acda050
Obinna Obianom <idonshayo@gmail.com>
2025-04-09 22:30:02 +0000

    version 1.0.7

--- R/OR.R
+++ R/OR.R
@@ -44,6 +44,7 @@
 #'
 #' @export
 or <- function(test,alternative){
+  error.out(test, return(alternative))
   res <- test
   if(!length(res)) return(alternative)
   if(all(is.null(res)|is.na(res) | is.nan(res) | !not.empty(res)))
